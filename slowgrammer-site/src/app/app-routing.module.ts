c60d563f2a7cb24a5d0f1ef626b34605d895f5c5
cristian hermida <cristian@USF-Gold-Wifi-226-127-159.laptops.usf.edu>
2018-03-08 16:40:42 -0500

    added components shop and home. added routing to those components

--- slowgrammer-site/src/app/app-routing.module.ts
+++ slowgrammer-site/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
+import { HomeComponent } from './component/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './component/shop/shop.component';
 
+export const routes: Routes = [
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'shop', component: ShopComponent}
+];
 
 @NgModule({
+  imports: [ RouterModule.forRoot(routes)],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
