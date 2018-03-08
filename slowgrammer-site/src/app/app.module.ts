2b111c51120ab653d3a5a336cace866da8d2ac75
jgmacho <jgmacho48@gmail.com>
2018-03-08 11:24:00 -0500

    moved component site details

--- slowgrammer-site/src/app/app.module.ts
+++ slowgrammer-site/src/app/app.module.ts
@@ -3,22 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { SiteDetailComponent } from './company-info/site-detail/site-detail.component';
 import { AppRoutingModule } from './/app-routing.module';
-=======
 import { NavBarComponent } from './component/nav-bar/nav-bar.component';
->>>>>>> 92a2617b1daf3cc6052f23912628c6d7a09eb94e
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    SiteDetailComponent
-=======
+    SiteDetailComponent,
     NavBarComponent
->>>>>>> 92a2617b1daf3cc6052f23912628c6d7a09eb94e
   ],
   imports: [
     BrowserModule,
